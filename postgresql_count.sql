42c71075a07b58e4f11293a26f3921347711a257
milo-bb <41932666+milo-bb@users.noreply.github.com>
2018-10-10 10:53:38 +0900

    Add files via upload

---
+++ postgresql_count.sql
@@ -0,0 +1,16 @@
+--業務日時
+\set :dtsys
+--出力ファイル名
+\set file count_ :dtsys .tsv
+
+\pset format unaligned
+\pset tuples_only on
+\pset fieldsep '\t'
+
+\o :file
+
+select count(*) from test;
+
+
+\o
+
